1a533bea99200b5c67ce1d5dc7f0c0d3bdab647f
Nicole Satiembre <80201174+LeNikki@users.noreply.github.com>
2021-08-02 12:48:42 +0800

    Delete app1 directory

--- app1/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import App1Students
-# Register your models here.
-
-admin.site.register(App1Students)
--- app1/filters.py
+++
@@ -1,7 +0,0 @@
-import django_filters
-from .models import *
-
-class StdFilter (django_filters.FilterSet):
-    class Meta:
-        model = App1Students
-        fields = '__all__'
